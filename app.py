2c0c8c372e34c6782817175b562854843f8cd299
Abhay Shah <shahabhay941@gmail.com>
2021-03-09 00:53:55 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
