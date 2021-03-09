9e8fb3783507bfc6cec9f99947253a34a0eb3777
Abhay Shah <shahabhay941@gmail.com>
2021-03-09 20:00:15 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length, email
+from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 import pandas as pd
 import pickle
 import numpy as np
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+filename = 'diabetes-prediction-rfc-model.pkl'
+classifier = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -156,6 +161,62 @@ def predict():
         res_val = "no Breast cancer"
 
     return render_template('cancer.html', prediction_text='Patient has {}'.format(res_val))
+
+
+##################################################################################
+
+df1 = pd.read_csv('diabetes.csv')
+
+# Renaming DiabetesPedigreeFunction as DPF
+df1 = df1.rename(columns={'DiabetesPedigreeFunction': 'DPF'})
+
+# Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
+df_copy = df1.copy(deep=True)
+df_copy[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = df_copy[['Glucose', 'BloodPressure',
+                                                                                    'SkinThickness', 'Insulin',
+                                                                                    'BMI']].replace(0, np.NaN)
+
+# Replacing NaN value by mean, median depending upon distribution
+df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
+df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
+df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
+df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
+df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
+
+# Model Building
+
+X = df1.drop(columns='Outcome')
+y = df1['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Creating Random Forest Model
+
+classifier = RandomForestClassifier(n_estimators=20)
+classifier.fit(X_train, y_train)
+
+# Creating a pickle file for the classifier
+filename = 'diabetes-prediction-rfc-model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+
+#####################################################################
+
+
+@app.route('/predictt', methods=['POST'])
+def predictt():
+    if request.method == 'POST':
+        preg = request.form['pregnancies']
+        glucose = request.form['glucose']
+        bp = request.form['bloodpressure']
+        st = request.form['skinthickness']
+        insulin = request.form['insulin']
+        bmi = request.form['bmi']
+        dpf = request.form['dpf']
+        age = request.form['age']
+
+        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+        my_prediction = classifier.predict(data)
+
+        return render_template('diab_result.html', prediction=my_prediction)
 
 
 if __name__ == "__main__":
