5db23da87fffb5f448456a9e117ea466e714b51e
Abhay Shah <shahabhay941@gmail.com>
2021-04-02 02:08:17 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -219,6 +219,30 @@ def predictt():
         return render_template('diab_result.html', prediction=my_prediction)
 
 
+############################################################################################################
+
+@app.route('/predictheart', methods=['POST'])
+def predictheart():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+
+    features_name = ["age", "trestbps", "chol", "thalach", "oldpeak", "sex_0",
+                     "  sex_1", "cp_0", "cp_1", "cp_2", "cp_3", "  fbs_0",
+                     "restecg_0", "restecg_1", "restecg_2", "exang_0", "exang_1",
+                     "slope_0", "slope_1", "slope_2", "ca_0", "ca_1", "ca_2", "thal_1",
+                     "thal_2", "thal_3"]
+
+    df = pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(df)
+
+    if output == 1:
+        res_val = "** heart disease **"
+    else:
+        res_val = "no heart disease "
+
+    return render_template('heart_result.html', prediction_text='Patient has {}'.format(res_val))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
