3f51b726420f60c27e3a31603a9f274c3b039085
Abhay Shah <shahabhay941@gmail.com>
2021-04-02 12:08:49 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
