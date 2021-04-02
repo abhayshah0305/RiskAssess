24968ac9b795a473b68de7576decbf4c5651ce3a
Abhay Shah <shahabhay941@gmail.com>
2021-04-02 14:42:15 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -164,6 +164,30 @@ def liver():
     return render_template("liver.html")
 
 
+def ValuePred(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    if(size==7):
+        loaded_model = joblib.load('liver_model.pkl')
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/predictliver', methods=["POST"])
+def predictliver():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        if len(to_predict_list) == 7:
+            result = ValuePred(to_predict_list, 7)
+
+    if int(result) == 1:
+        prediction = "Sorry you chances of getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return render_template("liver_result.html", prediction_text=prediction)
+
+
 @app.route('/logout')
 @login_required
 def logout():
